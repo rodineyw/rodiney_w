f6d6a815ce41d90f0cb26546179f0e9005dce0a2
rodineyw <rodineyw@yahoo.com.br>
2023-11-21 20:09:37 -0300

    fix

--- app/Pages/Contato/page.tsx
+++ app/Pages/Contato/page.tsx
@@ -11,7 +11,7 @@ export default function Contato() {
    <div className="flex flex-col mt-8 text-slate-400 justify-center items-center gap-4">
     <a href="mailto:rodineyw@yahoo.com.br" className="flex text-xl bg-slate-800 items-center justify-center gap-2 rounded-full px-8 py-2"><RxEnvelopeClosed className="text-4xl" /> rodineyw@yahoo.com.br</a>
 
-    <a href="mailto:rodineyw@yahoo.com.br" className="flex w-48 text-xl bg-slate-800 items-center justify-center gap-2 rounded-full px-4 py-2"><RxLinkedinLogo className="text-4xl" /> LinkedIn</a>
+    <a target="_blank" href="https://www.linkedin.com/in/rodineyw/" className="flex w-48 text-xl bg-slate-800 items-center justify-center gap-2 rounded-full px-4 py-2"><RxLinkedinLogo className="text-4xl" /> LinkedIn</a>
    </div>
   </div>
  )
--- app/Pages/Home/page.tsx
+++ app/Pages/Home/page.tsx
@@ -1,42 +1,42 @@
 import Image from 'next/image';
 import { RxGithubLogo, RxLinkedinLogo } from "react-icons/rx";
 export default function Home() {
- return (
-  <div className="relative h-screen justify-center items-center m-4 p-4">
+  return (
+    <div className="relative h-screen justify-center items-center m-4 p-4">
 
-   <div className='flex w-full rounded-full justify-center items-center '>
-    <Image className='rounded-full w-48'
-     src='/perfil.jpeg'
-     alt='Foto de perfil de Ródiney Wanderson, criador do site'
-     width={100}
-     height={100}
-    /></div>
+      <div className='flex w-full rounded-full justify-center items-center '>
+        <Image className='rounded-full w-48'
+          src='/perfil.jpeg'
+          alt='Foto de perfil de Ródiney Wanderson, criador do site'
+          width={100}
+          height={100}
+        /></div>
 
-   <div className='flex flex-col gap-2 mt-8 items-center w-full'>
-    <h1 className="text-2xl font-light text-slate-500">Oi, eu sou</h1>
-    <h1 className="text-4xl font-extrabold bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 text-transparent bg-clip-text">Ródiney Wanderson. </h1>
-    <h1 className="text-2xl font-light text-slate-500">
-     Front-End Developer
-    </h1>
-   </div>
+      <div className='flex flex-col gap-2 mt-8 items-center w-full'>
+        <h1 className="text-2xl font-light text-slate-500">Oi, eu sou</h1>
+        <h1 className="text-4xl text-center font-extrabold bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 text-transparent bg-clip-text">Ródiney Wanderson.</h1>
+        <h1 className="text-2xl font-light text-slate-500">
+          Front-End Developer
+        </h1>
+      </div>
 
 
-   <div className='flex w-full gap-4 justify-center mt-4'>
+      <div className='flex w-full gap-4 justify-center mt-4'>
 
-    <div className='flex'>
-     <RxLinkedinLogo className="text-slate-500 text-4xl" />
-     <p className='text-slate-500'></p>
+        <div className='flex'>
+          <a target="_blank" href="https://www.linkedin.com/in/rodineyw/">
+            <RxLinkedinLogo className="text-slate-500 text-4xl" /></a>
+        </div>
+        <div className='flex'>
+          <a target="_blank" href="https://github.com/rodineyw">
+            <RxGithubLogo className="text-slate-500 text-4xl" /></a>
+        </div>
+      </div>
+
+      <div className='flex justify-center items-center py-10'>
+        <button className='text-xl text-slate-900 hover:text-slate-200 hover:underline bg-slate-500 hover:bg-blue-600 p-4 rounded-full font-bold'>
+          <a href="#contato">Informações de Contato</a></button>
+      </div>
     </div>
-    <div className='flex'>
-     <RxGithubLogo className="text-slate-500 text-4xl" />
-     <p className='text-slate-500'></p>
-    </div>
-   </div>
-
-   <div className='flex justify-center items-center py-10'>
-    <button className='text-xl text-slate-900 hover:text-slate-200 hover:underline bg-slate-500 hover:bg-blue-600 p-4 rounded-full font-bold'>
-     <a href="#contato">Informações de Contato</a></button>
-   </div>
-  </div>
- )
+  )
 }
