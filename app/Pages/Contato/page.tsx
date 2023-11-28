d96fda4549bb46663d46ca3f067b6c8930386950
rodineyw <rodineyw@yahoo.com.br>
2023-11-28 07:11:34 -0300

    fix

--- app/Pages/Contato/page.tsx
+++ app/Pages/Contato/page.tsx
@@ -10,9 +10,9 @@ export default function Contato() {
 
    <div className="flex flex-col mt-8 text-slate-800 dark:text-slate-400 justify-center items-center gap-4">
     <a href="mailto:rodineyw@yahoo.com.br" className="flex text-xl 
-   bg-slate-400 hover:bg-blue-500 transition-all duration-500 ease-out dark:bg-slate-800 items-center justify-center gap-2 rounded-full px-8 py-2"><RxEnvelopeClosed className="text-4xl" /> rodineyw@yahoo.com.br</a>
+   bg-slate-400 hover:bg-blue-500 transition-all duration-500 ease-out dark:bg-slate-800 hover:text-slate-100 transition-all duration-500 ease-out items-center justify-center gap-2 rounded-full px-8 py-2"><RxEnvelopeClosed className="text-4xl" /> rodineyw@yahoo.com.br</a>
 
-    <a target="_blank" href="https://www.linkedin.com/in/rodineyw/" className="flex w-48 text-xl bg-slate-400 hover:bg-blue-500 transition-all duration-500 ease-out dark:bg-slate-800 items-center justify-center gap-2 rounded-full px-4 py-2"><RxLinkedinLogo className="text-4xl" /> LinkedIn</a>
+    <a target="_blank" href="https://www.linkedin.com/in/rodineyw/" className="flex w-48 text-xl bg-slate-400 hover:bg-blue-500 transition-all duration-500 ease-out dark:bg-slate-800 hover:text-slate-100 transition-all duration-500 ease-out items-center justify-center gap-2 rounded-full px-4 py-2"><RxLinkedinLogo className="text-4xl" /> LinkedIn</a>
    </div>
   </div>
  )
